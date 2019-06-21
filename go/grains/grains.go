4d18be8d5ddd3897bd7e1a4afc658e3bcb51f46d
Mike Waldner <mike.waldner@gmail.com>
2019-06-21 08:36:36 -0600

    Simplified, a lot...

--- go/grains/grains.go
+++ go/grains/grains.go
@@ -2,36 +2,14 @@ package grains
 
 import "fmt"
 
-func SquareTotal(n int) (uint64, uint64, error) {
-	if n < 0 {
-		return 0, 0, fmt.Errorf("Cant use negative")
-	}
-
-	if n == 0 {
-		return 0, 0, fmt.Errorf("Cant use zero")
-	}
-
-	var sum uint64 = 1
-	var square uint64 = 1
-
-	for i := 0; i < n-1; i++ {
-		square <<= 1
-		sum += square
-	}
-	return sum, square, nil
-}
-
 func Square(n int) (uint64, error) {
-
-	if n > 64 {
-		return 0, fmt.Errorf("Cant use > 64")
+	if n < 1 || n > 64 {
+		return 0, fmt.Errorf("Must be between 1 and 64")
 	}
 
-	_, square, err := SquareTotal(n)
-	return square, err
+	return 1 << uint(n-1), nil
 }
 
 func Total() uint64 {
-	sum, _, _ := SquareTotal(64)
-	return sum
+	return 1<<uint(64) - 1
 }
